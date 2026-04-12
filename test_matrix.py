aacca0b4231d9366a3a0ff14d0fa94750db58aca
Vladislav <143926973+SmileVladislav@users.noreply.github.com>
2026-04-12 18:37:19 +0300

    Update test_matrix.py

--- test_matrix.py
+++ test_matrix.py
@@ -1,20 +1,25 @@
 import unittest
-from matrix import multiply_by_scalar, calculate_specific_sum
+from matrix import Matrix
 
 class TestMatrixLab(unittest.TestCase):
     def test_multiply_scalar(self):
-        matrix = [[1.0, 2.0], [3.0, 4.0]]
-        result = multiply_by_scalar(matrix, 2.0)
+        
+        initial_data = [[1.0, 2.0], [3.0, 4.0]]
+        matrix_obj = Matrix(initial_data)
+        result = matrix_obj.multiply_by_scalar(2.0)
         self.assertEqual(result, [[2.0, 4.0], [6.0, 8.0]])
 
     def test_specific_sum(self):
-        matrix = [[10.0, 5.0], [1.0, 2.0]]
-        result = calculate_specific_sum(matrix)
+       
+        matrix_data = [[10.0, 5.0], [1.0, 2.0]]
+        matrix_obj = Matrix(matrix_data)
+        result = matrix_obj.calculate_variant_sum()
         self.assertEqual(result, 12.0)
 
     def test_empty_matrix(self):
+        
         with self.assertRaises(ValueError):
-            multiply_by_scalar([], 5.0)
+            Matrix([])
 
 if __name__ == "__main__":
     unittest.main()
